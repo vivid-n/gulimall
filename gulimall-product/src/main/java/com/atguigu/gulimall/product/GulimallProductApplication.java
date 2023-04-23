ef501f2af4854ff8991e0cf5f964337d11ee3c89
vivi <179286275@qq.com>
2023-04-23 22:13:26 +0800

    配置微服务

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,5 +1,6 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
@@ -11,11 +12,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *             <version>3.3.1</version>
  *         </dependency>
  *    2）配置
- *      1.配置数据
+ *      1.配置数据源
+ *          1)导入数据驱动
+ *          2）
+ *      2.配置MyBatis-Plus
  *
  */
-/
 
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
